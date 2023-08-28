169d9cc7426158e0aac0e4e1fa7d430ade4e445f
Mylène Mariana Gonzales André <mariana.gonzales.andre@gmail.com>
2023-08-28 19:19:41 +0200

    fix summary

--- bin/summary_hash.py
+++ bin/summary_hash.py
@@ -297,64 +297,64 @@ def demuxmix_summary(demuxmix_res,raw_adata, raw_mudata):
     classi = []
     assign = []
     params = []
-    
-    for x in demuxmix_res:
-        obs_res_dir = os.path.join(x, [filename for filename in os.listdir(x) if filename.endswith("_assignment_demuxmix.csv")][0])
-        demuxmix_asign = pd.read_csv(obs_res_dir)
-        if demuxmix_asign.empty:
-            #no results create empty dataframe for empty col
-            column_names = ['Barcode', os.path.basename(x)]
-            # Create an empty dataframe with only column names
-            df = pd.DataFrame(columns=column_names)
-            classi.append(df)
-            assign.append(df)
-        else:
-            dt_assign = demuxmix_asign[["Barcode", "HTO"]]
-            dt_assign.columns = ["Barcode", os.path.basename(x)]
-            assign.append(dt_assign)
+    files_in_folder = [item for item in demuxmix_res if os.path.isfile(os.path.join(demuxmix_res, item))]
 
-            if raw_adata is not None:
-                adata = raw_adata.copy()
-                adata.obs = adata.obs.merge(demuxmix_asign, left_index=True, right_on='Barcode', how='left').set_index('Barcode')
-                adata.obs.rename(columns={adata.obs.columns[0]: 'donor'}, inplace=True)
-                adata.obs.donor = adata.obs.donor.fillna("negative")
-                adata.obs.donor = adata.obs.donor.astype(str)
-                adata.write("hash_summary/adata/adata_with_"+os.path.basename(x)+".h5ad")
+    if len(files_in_folder) > 0:
+        for x in demuxmix_res:
+            obs_res_dir = os.path.join(x, [filename for filename in os.listdir(x) if filename.endswith("_assignment_demuxmix.csv")][0])
+            demuxmix_asign = pd.read_csv(obs_res_dir)
+            if demuxmix_asign.empty:
+                #no results create empty dataframe for empty col
+                column_names = ['Barcode', os.path.basename(x)]
+                # Create an empty dataframe with only column names
+                df = pd.DataFrame(columns=column_names)
+                classi.append(df)
+                assign.append(df)
+            else:
+                dt_assign = demuxmix_asign[["Barcode", "HTO"]]
+                dt_assign.columns = ["Barcode", os.path.basename(x)]
+                assign.append(dt_assign)
 
-            if raw_mudata is not None:
-                mudata = raw_mudata.copy()
-                mudata['rna'].obs = mudata['rna'].obs.merge(demuxmix_asign, left_index=True, right_on='Barcode', how='left').set_index('Barcode')
-                mudata['rna'].obs.rename(columns={mudata['rna'].obs.columns[0]: 'donor'}, inplace=True)
-                mudata['rna'].obs.donor = mudata['rna'].obs.donor.fillna("negative")
-                mudata['rna'].obs.donor = mudata['rna'].obs.donor.astype(str)
-                mudata.update()
-                mudata.write("hash_summary/mudata/mudata_with_mudata_"+ os.path.basename(x)+".h5mu") 
+                if raw_adata is not None:
+                    adata = raw_adata.copy()
+                    adata.obs = adata.obs.merge(demuxmix_asign, left_index=True, right_on='Barcode', how='left').set_index('Barcode')
+                    adata.obs.rename(columns={adata.obs.columns[0]: 'donor'}, inplace=True)
+                    adata.obs.donor = adata.obs.donor.fillna("negative")
+                    adata.obs.donor = adata.obs.donor.astype(str)
+                    adata.write("hash_summary/adata/adata_with_"+os.path.basename(x)+".h5ad")
 
-            demuxmix_classi = pd.read_csv(obs_res_dir)
-            dt_classi = demuxmix_classi[["Barcode", "Classification"]]
-            dt_classi.columns = ["Barcode", os.path.basename(x)]
-            classi.append(dt_classi) 
+                if raw_mudata is not None:
+                    mudata = raw_mudata.copy()
+                    mudata['rna'].obs = mudata['rna'].obs.merge(demuxmix_asign, left_index=True, right_on='Barcode', how='left').set_index('Barcode')
+                    mudata['rna'].obs.rename(columns={mudata['rna'].obs.columns[0]: 'donor'}, inplace=True)
+                    mudata['rna'].obs.donor = mudata['rna'].obs.donor.fillna("negative")
+                    mudata['rna'].obs.donor = mudata['rna'].obs.donor.astype(str)
+                    mudata.update()
+                    mudata.write("hash_summary/mudata/mudata_with_mudata_"+ os.path.basename(x)+".h5mu") 
 
-            params_dir = os.path.join(x, [filename for filename in os.listdir(x) if filename == "params.csv"][0])
-            params_res = pd.read_csv(params_dir, usecols=[1, 2], keep_default_na=False, index_col=0)     
-            params_res.columns = [os.path.basename(x)]
-            params.append(params_res)
+                demuxmix_classi = pd.read_csv(obs_res_dir)
+                dt_classi = demuxmix_classi[["Barcode", "Classification"]]
+                dt_classi.columns = ["Barcode", os.path.basename(x)]
+                classi.append(dt_classi) 
 
-    classi_df = pd.concat(classi, axis=1, join="outer")
-    classi_df.to_csv("hash_summary" + "/demuxmix_classification.csv",index=False)
-    
-    assign_df = pd.concat(assign, axis=1, join="outer")
-    assign_df.to_csv("hash_summary"  +"/demuxmix_assignment.csv",index=False)
+                params_dir = os.path.join(x, [filename for filename in os.listdir(x) if filename == "params.csv"][0])
+                params_res = pd.read_csv(params_dir, usecols=[1, 2], keep_default_na=False, index_col=0)     
+                params_res.columns = [os.path.basename(x)]
+                params.append(params_res)
 
-    params = pd.concat(params, axis=1)
-    params.to_csv("hash_summary"  +"/demuxmix_params.csv",index=False)
+        classi_df = pd.concat(classi, axis=1, join="outer")
+        classi_df.to_csv("hash_summary" + "/demuxmix_classification.csv",index=False)
+        
+        assign_df = pd.concat(assign, axis=1, join="outer")
+        assign_df.to_csv("hash_summary"  +"/demuxmix_assignment.csv",index=False)
 
+        params = pd.concat(params, axis=1)
+        params.to_csv("hash_summary"  +"/demuxmix_params.csv",index=False)`
 
 def gmm_summary(gmmDemux_res,raw_adata, raw_mudata):
     classi = []
     assign = []
     params = []
-
     for x in gmmDemux_res:
         obs_res_dir = os.path.join(x, [filename for filename in os.listdir(x) if filename.endswith("GMM_full.csv")][0])
         
