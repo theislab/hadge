749f8be933d35c95b177af9d52760362dff8439e
Mylène Mariana Gonzales André <mariana.gonzales.andre@gmail.com>
2023-11-06 19:24:41 +0100

    correction to merged files

--- bin/summary_hash.py
+++ bin/summary_hash.py
@@ -450,13 +450,13 @@ def bff_summary(bff_res,raw_adata, raw_mudata):
                     dt_assign = dt_assign.drop([column], axis=1)
             dt_assign.loc[dt_assign["consensuscall"] == "Singlet", "consensuscall"] = "singlet"
             dt_assign.loc[dt_assign["consensuscall"] == "Doublet", "consensuscall"] = "doublet"
+            dt_assign.loc[dt_assign["consensuscall"] == "Negative", "consensuscall"] = "negative"
+            dt_assign['consensuscall'] = dt_assign['consensuscall'].astype('category')
             dt_assign = dt_assign.rename(columns={"cellbarcode": "Barcode", "consensuscall": os.path.basename(x)})
             assign.append(dt_assign)
             if raw_adata is not None:
                 adata = raw_adata.copy()
-
                 adata.obs = adata.obs.merge(dt_assign, left_index=True, right_index=True, how='left')
-
                 adata.obs.rename(columns={adata.obs.columns[0]: 'donor'}, inplace=True)
                 adata.obs.donor = adata.obs.donor.fillna("negative")
                 adata.obs.donor = adata.obs.donor.astype(str)
@@ -465,7 +465,6 @@ def bff_summary(bff_res,raw_adata, raw_mudata):
 
             if raw_mudata is not None:
                 mudata = raw_mudata.copy()
-
                 mudata['rna'].obs = mudata['rna'].obs.merge(dt_assign, left_index=True, right_index=True, how='left')
                 mudata['rna'].obs.rename(columns={mudata['rna'].obs.columns[0]: 'donor'}, inplace=True)
                 mudata['rna'].obs.donor = mudata['rna'].obs.donor.fillna("negative")
