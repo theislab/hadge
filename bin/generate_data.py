db963eac0db390bce24d5e60acaabd9fa7cea996
wxicu <xichenwu@outlook.com>
2024-03-15 14:09:27 +0800

    fix generate anndata bug

--- bin/generate_data.py
+++ bin/generate_data.py
@@ -28,48 +28,53 @@ parser.add_argument(
 args = parser.parse_args()
 
 if __name__ == "__main__":
+    assignment_dir = os.path.join(
+        args.assignment,
+        [
+            filename
+            for filename in os.listdir(args.assignment)
+            if filename == "all_assignment_after_match.csv"
+        ][0],
+    )
     if args.generate_anndata:
-        adata = sc.read_10x_mtx(args.read_rna_mtx)
-        assignment_dir = os.path.join(
-            args.assignment,
-            [
-                filename
-                for filename in os.listdir(args.assignment)
-                if filename == "all_assignment_after_match.csv"
-            ][0],
-        )
-
-        assignment = pd.read_csv(assignment_dir, index_col=0)
-        adata.obs = adata.obs.merge(
-            assignment, left_index=True, right_index=True, how="left"
-        )
-        adata.obs = adata.obs.fillna("negative")
-        adata.obs[adata.obs.columns[0]] = adata.obs[adata.obs.columns[0]].astype(str)
-        adata.obs[adata.obs.columns[1]] = adata.obs[adata.obs.columns[1]].astype(str)
-        adata.write("adata_with_donor_matching.h5ad")
+        if os.path.isfile(assignment_dir):
+            adata = sc.read_10x_mtx(args.read_rna_mtx)
+            assignment = pd.read_csv(assignment_dir, index_col=0)
+            adata.obs = adata.obs.merge(
+                assignment, left_index=True, right_index=True, how="left"
+            )
+            adata.obs = adata.obs.fillna("negative")
+            adata.obs[adata.obs.columns[0]] = adata.obs[adata.obs.columns[0]].astype(
+                str
+            )
+            adata.obs[adata.obs.columns[1]] = adata.obs[adata.obs.columns[1]].astype(
+                str
+            )
+            adata.write("adata_with_donor_matching.h5ad")
+        else:
+            print(
+                "Can not find assignment file, stop generating anndata. Please ensure the donor matching process has been executed to match hashing-based and genotyped-based methods."
+            )
 
     if args.generate_mudata:
-        rna_data = sc.read_10x_mtx(args.read_rna_mtx)
-        hto_data = sc.read_10x_mtx(args.read_hto_mtx, gex_only=False)
-        assignment_dir = os.path.join(
-            args.assignment,
-            [
-                filename
-                for filename in os.listdir(args.assignment)
-                if filename == "all_assignment_after_match.csv"
-            ][0],
-        )
+        if os.path.isfile(assignment_dir):
+            rna_data = sc.read_10x_mtx(args.read_rna_mtx)
+            hto_data = sc.read_10x_mtx(args.read_hto_mtx, gex_only=False)
 
-        assignment = pd.read_csv(assignment_dir, index_col=0)
-        mudata = MuData({"rna": rna_data, "hto": hto_data})
+            assignment = pd.read_csv(assignment_dir, index_col=0)
+            mudata = MuData({"rna": rna_data, "hto": hto_data})
 
-        mudata["rna"].obs = mudata["rna"].obs.merge(
-            args.assignment, left_index=True, right_index=True, how="left"
-        )
-        mudata["rna"].obs.rename(
-            columns={mudata["rna"].obs.columns[0]: "donor"}, inplace=True
-        )
-        mudata["rna"].obs.donor = mudata["rna"].obs.donor.fillna("negative")
-        mudata["rna"].obs.donor = mudata["rna"].obs.donor.astype(str)
-        mudata.update()
-        mudata.write("mudata_with_donor_matching.h5mu")
+            mudata["rna"].obs = mudata["rna"].obs.merge(
+                args.assignment, left_index=True, right_index=True, how="left"
+            )
+            mudata["rna"].obs.rename(
+                columns={mudata["rna"].obs.columns[0]: "donor"}, inplace=True
+            )
+            mudata["rna"].obs.donor = mudata["rna"].obs.donor.fillna("negative")
+            mudata["rna"].obs.donor = mudata["rna"].obs.donor.astype(str)
+            mudata.update()
+            mudata.write("mudata_with_donor_matching.h5mu")
+        else:
+            print(
+                "Can not find assignment file, stop generating anndata. Please ensure the donor matching process has been executed to match hashing-based and genotyped-based methods."
+            )
